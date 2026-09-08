a2738281de6e5bae3df7ca388c473b0198daae1a
JasonAltamiranda <jason_mza_94@hotmail.com>
2026-09-08 20:47:27 -0300

    inicializacion spring boot

---
+++ src/main/java/ies/belgrano/lotes/LotesApplication.java
@@ -0,0 +1,13 @@
+package ies.belgrano.lotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ies/belgrano/lotes/LotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.belgrano.lotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
